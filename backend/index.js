0e3815d921bd7e0c1a6fb71543852c4b36f91e95
Kevin Liu <kevinliu676@yahoo.ca>
2023-05-09 23:55:22 -0400

    working on backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,55 @@
+// import mongodb from "mongodb";
 import app from "./server.js";
-import mongodb from "mongodb";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
-const MongoClient = mongodb.MongoClient;
 
 const port = process.env.PORT || 4000;
+const uri = process.env.FIRESIDE_DB_URI;
 
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    wtimeout: 2500,
+    useUnifiedTopology : true 
+}) .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+}) .then(() => {
+    app.listen(port, () => {
+        console.log(`Server is listening port ${port}`);
+    });
+}); 
+
+
+
+
+/*
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established succesfully");
+})
+*/
+
+/*
+
+// Implementation w/ official MongoDB driver
+
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(
+    uri,
+    {
+        wtimeout: 2500,
+        useNewUrlParse: true
+    })
+    .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+        })
+    }) 
+*/
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
-// import other stuff
+import trips from "./api/trips.route.js";
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1/");
+const tripsRouter = require('./api/trips.route.js');
+
+app.use("/api/v1/trips", tripsRouter);
+app.use("*", (req, res) => res.status(404).json({ error: "Oops. Looks like you took a wrong turn somewhere!"}));
 
 export default app;
